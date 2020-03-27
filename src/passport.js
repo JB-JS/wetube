5adf0caa8a0610552b2618a3a75090a513162130
bumsuejung <y011010@naver.com>
2020-03-28 03:03:50 +0900

    address

--- src/passport.js
+++ src/passport.js
@@ -14,7 +14,7 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`
+      callbackURL: `https://wetube-j.herokuapp.com${routes.githubCallback}`
     },
     githubLoginCallback
   )
